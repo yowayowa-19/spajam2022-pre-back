ac8cafa67c5a84263ac122397ce1248f2b189c09
materialofmouse <materialofmouse@gmail.com>
2022-08-21 02:28:38 +0900

    add: ranking, missions fix: endpoint

--- src/routers/missions.py
+++ src/routers/missions.py
@@ -1,8 +1,26 @@
 from fastapi import APIRouter
 
+from pydantic import BaseModel
+
+from typing import List, Union
+
+
+class Mission(BaseModel):
+    title: str
+    describe: str
+    point: int
+    status: bool
+
+
+class Missions(BaseModel):
+    weekly: List[Mission]
+    daily: List[Mission]
+
+
 router = APIRouter()
 
 
-@router.get("/missions/{user_id}")
-async def get_missions():
-    return 
+@router.get("/missions/{user_id}", response_model=Missions)
+async def get_missions(user_id: int):
+    missions = Missions
+    return missions
