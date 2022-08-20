ee401e60a3dd7b063e54922e39885fae09acc787
t4t5u0 <ymmtryk0902@gmail.com>
2022-08-20 23:48:32 +0900

    Update: 固定データをinitSQLにうつした

---
+++ Docker/postgres/initdb/00startup.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS user_table (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    email varchar(255),
+    region int,
+    has_car boolean,
+    has_bike boolean,
+    has_aircon boolean,
+    has_tv boolean,
+    total_points int
+);
+
+CREATE TABLE IF NOT EXISTS daily_mission_table (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    content varchar(255),
+    tag_id int,
+    point int
+);
+
+CREATE TABLE IF NOT EXISTS weekly_mission_table (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    content varchar(255),
+    tag_id int,
+    point int
+);
+
+CREATE TABLE IF NOT EXISTS daily_history_table (
+    id serial PRIMARY KEY,
+    completed_at datetime,
+    user_id int,
+    mission_id int
+);
+
+CREATE TABLE IF NOT EXISTS weekly_history_table (
+    id serial PRIMARY KEY,
+    completed_at datetime,
+    user_id int,
+    mission_id int
+);
+
+CREATE TABLE IF NOT EXISTS all_history_table (
+    id serial PRIMARY KEY,
+    completed_at datetime,
+    user_id int,
+    mission_category str,
+    mission_id int
+);
+
+CREATE TABLE IF NOT EXISTS region_table (id serial PRIMARY KEY, name varchar(255));
+
+CREATE TABLE IF NOT EXISTS tag_table (id serial PRIMARY KEY, name varchar(255));
---
+++ Docker/postgres/initdb/01fixeddata.sql
@@ -0,0 +1,7 @@
+INSERT INTO daily_mission_table VALUES ();
+
+INSERT INTO weekly_mission_table VALUES ();
+
+INSERT INTO region_table VALUES ();
+
+INSERT INTO tag_table VALUES ();
