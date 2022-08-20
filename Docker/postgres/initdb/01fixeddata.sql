938eb7ace8be2169aa9d185d2e33fd4fc8a3712f
t4t5u0 <ymmtryk0902@gmail.com>
2022-08-21 02:03:34 +0900

    Update: 領域情報を初期化

--- Docker/postgres/initdb/01fixeddata.sql
+++ Docker/postgres/initdb/01fixeddata.sql
@@ -1,7 +1,29 @@
-INSERT INTO daily_mission_table VALUES ();
+INSERT INTO
+    daily_mission_table
+VALUES
+    ();
 
-INSERT INTO weekly_mission_table VALUES ();
+INSERT INTO
+    weekly_mission_table
+VALUES
+    ();
 
-INSERT INTO region_table VALUES ();
+INSERT INTO
+    region_table
+VALUES
+    (
+        (1, '北海道'),
+        (2, '東北'),
+        (3, '関東'),
+        (4, '中部'),
+        (5, '関西'),
+        (6, '中国'),
+        (7, '四国'),
+        (8, '九州'),
+        (9, '沖縄')
+    ) ON CONFLICT DO NOTHING;
 
-INSERT INTO tag_table VALUES ();
+INSERT INTO
+    tag_table
+VALUES
+    ();
